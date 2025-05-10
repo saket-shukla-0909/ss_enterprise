645a1c42298761a3790b5c3a8f6f3eef12aa064c
Saket Shukla <148642140+saket-shukla-0909@users.noreply.github.com>
2025-05-10 13:25:12 +0530

    third commit of this project

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,5 +1,5 @@
 // controllers/userController.js
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const User = require('../models/usersModel');
 const jwt = require('jsonwebtoken');
 const dotenv = require("dotenv").config();
--- mysequelize2.js
+++ mysequelize2.js
@@ -1,9 +1,10 @@
 const { Sequelize } = require('sequelize');
+const dotenv = require("dotenv").config();
 
 // Replace with your own database connection details
-const sequelize = new Sequelize('ss_enterprise', 'root', '', {
-  host: 'localhost', // or your database server
-  dialect: 'mysql', // change if using another database (e.g., 'postgres', 'sqlite', 'mssql')
+const sequelize = new Sequelize( process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,// or your database server
+    dialect: process.env.DB_DIALECT, // change if using another database (e.g., 'postgres', 'sqlite', 'mssql')
 });
 
 async function authenticate() {
